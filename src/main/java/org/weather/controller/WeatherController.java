5b910d2d639d285e4ede5b92d2f2403fc5b4f382
OlegTihii <YouDoontKnow>
2025-03-24 23:30:14 +0300

    delete: clean WeatherController

--- src/main/java/org/weather/controller/WeatherController.java
+++ src/main/java/org/weather/controller/WeatherController.java
@@ -1,28 +1,10 @@
 package org.weather.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("weather")
 public class WeatherController {
 
-    private final ObjectMapper objectMapper;
-
-    public WeatherController(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
-
-    @GetMapping
-    public String getWeather() throws JsonProcessingException {
-        return objectMapper.writeValueAsString(Map.of(
-                "temperature", "15°C",
-                "city", "Москва"
-        ));
-    }
 }
