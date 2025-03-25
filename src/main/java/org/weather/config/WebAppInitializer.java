5874e4eee9dc150c3ba4a3be67f5d48dc7c3875b
OlegTihii <YouDoontKnow>
2025-03-25 23:27:55 +0300

    refactor: any change

--- src/main/java/org/weather/config/WebAppInitializer.java
+++ src/main/java/org/weather/config/WebAppInitializer.java
@@ -1,12 +1,9 @@
 package org.weather.config;
 
 import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class WebAppInitializer implements WebApplicationInitializer {
