f5aed33eb58cb7d0b6992be86fd7eba787a7182d
OlegTihii <YouDoontKnow>
2025-03-25 21:19:38 +0300

    The initial commit

--- src/main/java/org/weather/controller/WeatherController.java
+++ src/main/java/org/weather/controller/WeatherController.java
@@ -1,10 +1,11 @@
 package org.weather.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("weather")
 public class WeatherController {
+
 
 }
