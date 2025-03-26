f662d9e0bba953d7417e6559deb9f086ba529616
OlegTihii <YouDoontKnow>
2025-03-27 01:29:27 +0300

    feat: add test date

--- src/main/java/org/weather/controller/stub/MainWeatherControllerStub.java
+++ src/main/java/org/weather/controller/stub/MainWeatherControllerStub.java
@@ -1,16 +1,32 @@
 package org.weather.controller.stub;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.weather.controller.MainWeatherController;
+import org.weather.dto.LocationDto;
+import org.weather.service.LocationService;
+import org.weather.service.stub.LocationServiceStub;
 
+import java.util.List;
+import java.util.Optional;
+
+@Controller
 public class MainWeatherControllerStub implements MainWeatherController {
+
+    private final LocationService locationServiceStub = new LocationServiceStub();
+
     @Override
-    public String findLocationByName(String name) {
-        return null;
+    public String findLocationByName(String city, Model model) {
+        List<LocationDto> cities = locationServiceStub.findLocationByName("Dublin");
+        model.addAttribute(cities);
+        return "homePage";
     }
 
     @Override
-    public String findLocationByCoordinates(double lat, double lon) {
-        return null;
+    public String findLocationByCoordinates(double lat, double lon, Model model) {
+        LocationDto locationByCoordinates = locationServiceStub.findLocationByCoordinates(33.3, 55.5);
+        model.addAttribute(locationByCoordinates);
+        return "homePage";
     }
 
     @Override
