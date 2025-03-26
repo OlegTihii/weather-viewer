9f2e3d212172946f4ee7aa0279e4433ffcb77fe7
OlegTihii <YouDoontKnow>
2025-03-27 01:33:40 +0300

    feat: add test data

--- src/main/java/org/weather/service/stub/LocationServiceStub.java
+++ src/main/java/org/weather/service/stub/LocationServiceStub.java
@@ -5,7 +5,6 @@ import org.weather.dto.LocationDto;
 import org.weather.service.LocationService;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class LocationServiceStub implements LocationService {
@@ -14,15 +13,15 @@ public class LocationServiceStub implements LocationService {
     @Override
     public List<LocationDto> findLocationByName(String name) {
 
-        return locations = List.of(LocationDto.builder().city("Dublin").build(),
-                LocationDto.builder().city("Moscow").build(),
-                LocationDto.builder().city("New Vasyuki").build());
+        return locations = List.of(LocationDto.builder().city("Dublin").country("RU").humidity((byte) 67).temperature((short) 10).build(),
+                LocationDto.builder().city("Moscow").country("RU").humidity((byte) 22).temperature((short) 17).build(),
+                LocationDto.builder().city("New Vasyuki").country("RU").humidity((byte) 43).temperature((short) 28).build());
 
     }
 
     @Override
     public LocationDto findLocationByCoordinates(double lat, double lon) {
-        return LocationDto.builder().city("Dublin").temperature((short) 22).build();
+        return LocationDto.builder().city("Dublin").temperature((short) 22).country("RU").humidity((byte) 22).build();
     }
 
 
