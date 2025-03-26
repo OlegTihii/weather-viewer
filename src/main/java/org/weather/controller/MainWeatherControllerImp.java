e67839d3c52e10afe8c958ba4097c8eae3184ab2
OlegTihii <YouDoontKnow>
2025-03-27 01:59:21 +0300

    feat:

--- src/main/java/org/weather/controller/MainWeatherControllerImp.java
+++ src/main/java/org/weather/controller/MainWeatherControllerImp.java
@@ -1,30 +1,30 @@
-package org.weather.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@Controller
-@RequestMapping("weather")
-public class MainWeatherControllerImp implements MainWeatherController {
-
-    @GetMapping
-    public String findLocationByName(@RequestParam("city") String city, Model model) {
-        return "homePage";
-    }
-
-    @GetMapping
-    public String findLocationByCoordinates(@RequestParam("lat") double latitude,
-                                            @RequestParam("lon") double longitude,
-                                            Model model) {
-
-        return "homePage";
-    }
-
-    @Override
-    public String deleteLocation(int id) {
-        return null;
-    }
-}
+//package org.weather.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//@Controller
+//@RequestMapping("weather")
+//public class MainWeatherControllerImp implements MainWeatherController {
+//
+//    @GetMapping
+//    public String findLocationByName(@RequestParam("city") String city, Model model) {
+//        return "homePage";
+//    }
+//
+//    @GetMapping
+//    public String findLocationByCoordinates(@RequestParam("lat") double latitude,
+//                                            @RequestParam("lon") double longitude,
+//                                            Model model) {
+//
+//        return "homePage";
+//    }
+//
+//    @Override
+//    public String deleteLocation(int id) {
+//        return null;
+//    }
+//}
--- src/main/java/org/weather/controller/stub/MainWeatherControllerStub.java
+++ src/main/java/org/weather/controller/stub/MainWeatherControllerStub.java
@@ -1,27 +1,34 @@
 package org.weather.controller.stub;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.weather.controller.MainWeatherController;
 import org.weather.dto.LocationDto;
 import org.weather.service.LocationService;
-import org.weather.service.stub.LocationServiceStub;
 
 import java.util.List;
 
 @Controller
 public class MainWeatherControllerStub implements MainWeatherController {
+    private final LocationService locationServiceStub;
 
-    private final LocationService locationServiceStub = new LocationServiceStub();
+    public MainWeatherControllerStub(LocationService locationServiceStub) {
+        this.locationServiceStub = locationServiceStub;
+    }
 
     @Override
-    public String findLocationByName(String city, Model model) {
+    @GetMapping("/")
+    public String findLocationByName(@RequestParam("city") String city, Model model) {
         List<LocationDto> cities = locationServiceStub.findLocationByName("Dublin");
         model.addAttribute(cities);
         return "homePage";
     }
 
     @Override
+    @GetMapping("/test")
     public String findLocationByCoordinates(double lat, double lon, Model model) {
         LocationDto locationByCoordinates = locationServiceStub.findLocationByCoordinates(33.3, 55.5);
         model.addAttribute(locationByCoordinates);
