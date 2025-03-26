a361937735f465799d303b3a028ff5cc260085ac
OlegTihii <YouDoontKnow>
2025-03-27 01:05:42 +0300

    feat: add dto classes

---
+++ src/main/java/org/weather/dto/LocationDto.java
@@ -0,0 +1,23 @@
+package org.weather.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class LocationDto {
+
+    private String city;
+    private BigDecimal  latitude;
+    private BigDecimal longitude;
+    private String country;
+    private short temperature;
+    private byte humidity;
+
+}
---
+++ src/main/java/org/weather/dto/UserDto.java
@@ -0,0 +1,14 @@
+package org.weather.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+
+    private String userName;
+}
