dd941b52e0f56e22b6ac394acd84e36b1e5ffdb0
OlegTihii <YouDoontKnow>
2025-03-27 01:17:10 +0300

    feat: add test data

--- src/main/java/org/weather/service/stub/LocationServiceStub.java
+++ src/main/java/org/weather/service/stub/LocationServiceStub.java
@@ -1,27 +1,30 @@
 package org.weather.service.stub;
 
+import org.springframework.stereotype.Service;
 import org.weather.dto.LocationDto;
 import org.weather.service.LocationService;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class LocationServiceStub implements LocationService {
     private List<LocationDto> locations;
 
     @Override
     public List<LocationDto> findLocationByName(String name) {
-        locations = List.of(LocationDto.builder().city("Dublin").build()
+
+        return locations = List.of(LocationDto.builder().city("Dublin").build()
                 , LocationDto.builder().city("Moscow").build()
                 , LocationDto.builder().city("New Vasyuki").build());
 
-        return locations;
     }
 
     @Override
     public Optional<LocationDto> findLocationByCoordinates(double lat, double lon) {
-        return Optional.empty();
+        return Optional.ofNullable(LocationDto.builder().city("Dublin").temperature((short) 22).build());
     }
+
 
     @Override
     public boolean addLocation(int id) {
